05f2011d3d141f3a4bc93fe88b16b70152de668c
junforever <dgalvaro@hotmail.com>
2024-10-14 14:21:46 +0000

    fix: project schema created

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+
+import { JournalApp } from './JournalApp'
+import './styles.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <StrictMode>
+      <JournalApp />
+    </StrictMode>
+  </BrowserRouter>
 )
